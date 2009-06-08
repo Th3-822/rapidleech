d187c07dc42c7fd1beb10e5cf6813b05c9b13086
barneytramble <barneytramble@0f90527e-9506-11de-8d2f-ebe662690e28>
2009-06-08 22:50:38 +0000

    [CHANGE] Updated hotfile.com.php to member version (thanks to Choppaholik)

--- trunk/hosts/upload/hotfile.com.php
+++ trunk/hosts/upload/hotfile.com.php
@@ -3,6 +3,23 @@
 <tr><td align=center>
 <?php 
 
+$hotfile_username="";  // username
+$hotfile_password="";  // password
+
+if ($hotfile_username && $hotfile_password){
+  $in=parse_url("http://hotfile.com/login.php");
+$post=array();
+$post["returnto"]="/";
+$post["user"]=$hotfile_username;
+$post["pass"]=$hotfile_password;
+$page = geturl($in["host"], $in["port"] ? $in["port"] : 80, $in["path"].($in["query"] ? "?".$in["query"] : ""), "http://hotfile.com/", 0, $post, 0, $_GET["proxy"],$pauth);	
+preg_match('/auth=\w{64}/i', $page, $ook);
+$cook=$ook[0];
+if(!$cook){
+html_error("Login Failed , Bad username/password combination.",0);
+}
+}
+
 $hserver=parse_url("http://hotfile.com/");
 $page=geturl($hserver['host'],"80","http://hotfile.com/");
 
@@ -12,7 +29,7 @@ $action = str_replace("\"","",$action);
  
  $url=parse_url($action);
       $post['submit']="Upload";
-	  $upfiles=upfile($url["host"],$url["port"] ? $url["port"] : 80, $url["path"].($url["query"] ? "?".$url["query"] : ""), "http://hotfile.com/", 0, $fpost, $lfile, $lname, "uploads[]");
+	  $upfiles=upfile($url["host"],$url["port"] ? $url["port"] : 80, $url["path"].($url["query"] ? "?".$url["query"] : ""), "http://hotfile.com/", $cook, $fpost, $lfile, $lname, "uploads[]");
 	  
  if(preg_match('/Location: *(.+)/', $upfiles, $redir)){
  $redirect=rtrim($redir["1"]);
@@ -24,6 +41,9 @@ $action = str_replace("\"","",$action);
   preg_match('/http:\/\/.+kill[^\'"]+/', $page, $del);
   $download_link = rtrim($dwn['0']);
   $delete_link = rtrim($del['0']);
+   
+ 
+  
       
 ?>
 
