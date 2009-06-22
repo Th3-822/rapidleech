fb89f0326c8e6355158471ea383fbafa64280613
05049pyj <05049pyj@0f90527e-9506-11de-8d2f-ebe662690e28>
2009-06-22 09:27:37 +0000

    [REMOVED] Deleted died hosts plugins.

--- trunk/hosts/upload/got-webspace.com.index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$upload_services[]="got-webspace.com";
-$max_file_size["got-webspace.com"]=4194;
-$page_upload["got-webspace.com"] = "got-webspace.com.php";  
-?>
--- trunk/hosts/upload/momupload.com.index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$upload_services[]="momupload.com";
-$max_file_size["momupload.com"]=500;
-$page_upload["momupload.com"] = "momupload.com.php";  
-?>
