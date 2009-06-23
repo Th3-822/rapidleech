5022e7df19a6cb46c62c64748f63c5656e44713f
barneytramble <barneytramble@0f90527e-9506-11de-8d2f-ebe662690e28>
2009-06-23 23:46:09 +0000

    [FEATURE] Choose between Youtube formats (&fmt=18 or 34, more to follow) [FIX] Updated youtube_com.php download plugin for new selector, and set &fmt=18 back to the default HQ method; also some minor edits to main.php needed [FIX] Updated hotfile_com.php (kaox), turboupload.com for member upload, and renamed turboupload free to turboupload.com_free.php [PLUGIN] Added uploadbox.com member upload plugin

--- trunk/hosts/upload/turboupload.com.index.php
+++ trunk/hosts/upload/turboupload.com.index.php
@@ -1,5 +1,5 @@
-<?php
-$upload_services[]="turboupload.com";
-$max_file_size["turboupload.com"]=100;
-$page_upload["turboupload.com"] = "turboupload.com.php";  
+<?php 
+$upload_services[] = "turboupload.com";
+$max_file_size["turboupload.com"] = 500;
+$page_upload["turboupload.com"] = "turboupload.com.php";
 ?>
---
+++ trunk/hosts/upload/uploadbox.com_member.index.php
@@ -0,0 +1,5 @@
+<?php
+$upload_services[]="uploadbox.com_member";
+$max_file_size["uploadbox.com_member"]=2000;
+$page_upload["uploadbox.com_member"] = "uploadbox.com_member.php";  
+?>
