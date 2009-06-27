4cad06d90c1b510b7d8202feccbe662228527cac
05049pyj <05049PYJ@gmail.com>
2009-06-27 14:52:15 +0000

    [BUG] Fixed list links showing links that are url encoded.

--- classes/options/list.php
+++ classes/options/list.php
@@ -7,7 +7,7 @@ function rl_list() {
 				$inCurrDir = strstr(dirname($file["name"]), ROOT_DIR) ? TRUE : FALSE;
 				if($inCurrDir) {
 					$Path = parse_url($PHP_SELF);
-					$Path = 'http://'.$_SERVER['HTTP_HOST'].substr($Path["path"], 0, strlen($Path["path"]) - strlen(strrchr($Path["path"], "/")));
+					$Path = 'http://'.urldecode($_SERVER['HTTP_HOST']).substr($Path["path"], 0, strlen($Path["path"]) - strlen(strrchr($Path["path"], "/")));
 					echo($Path.substr(dirname($file["name"]), strlen(ROOT_DIR))."/".basename($file["name"]).'<br />');
 				}
 			}
