8eeba2bd48accab520ad2bb6a68aa12a7b424f80
05049pyj <05049PYJ@gmail.com>
2009-07-03 11:36:27 +0000

    Fixed a problem where force_name cannot be passed. For example the youtube plugin.

--- hosts/DownloadClass.php
+++ hosts/DownloadClass.php
@@ -38,7 +38,18 @@ class DownloadClass {
 		return $page;
 	}
 	
-	public function RedirectDownload($link, $FileName, $cookie = 0, $post = 0, $referer = 0, $auth = "") {
+	/**
+	 * Use this function instead of insert_location so that we can improve this feature in the future
+	 * 
+	 * @param string $link The download link of the file
+	 * @param string $FileName The name of the file
+	 * @param string $cookie The cookie value
+	 * @param array $post The post value will be serialized here
+	 * @param string $referer The page that refered to this link
+	 * @param string $auth In format username:password
+	 * @param array $params This parameter allows you to add extra _GET values to be passed on
+	 */
+	public function RedirectDownload($link, $FileName, $cookie = 0, $post = 0, $referer = 0, $auth = "", $params = array()) {
 		global $pauth;
 		if (!$referer) {
 			global $Referer;
@@ -46,6 +57,20 @@ class DownloadClass {
 		}
 		$Url = parse_url($link);
 		if (substr($auth,0,6) != "&auth=") $auth = "&auth=" . $auth;
+		if (!is_array($params)) {
+			// Some problems with the plugin, quit it
+			html_error('Plugin problem! Please report, error: "The parameter passed must be an array"');
+		}
+		$addon = "";
+		if (count((array) $params) > 0) {
+			foreach ($params as $name => $value) {
+				if (is_array($value)) {
+					$value = serialize($value);
+				}
+				$addon .= '&'.$name.'='.urlencode($value).'&';
+			}
+			$addon = substr($addon,0,-1);
+		}
 		$loc = "{$_SERVER['PHP_SELF']}?filename=" . urlencode ( $FileName ) . 
 			"&host=" . $Url ["host"] . "&port=" . $Url ["port"] . "&path=" . 
 			urlencode ( $Url ["path"] . ($Url ["query"] ? "?" . $Url ["query"] : "") ) . 
@@ -59,7 +84,7 @@ class DownloadClass {
 			"&post=" . urlencode ( serialize ( $post ) ) .
 			($_POST ["uploadlater"] ? "&uploadlater=".$_POST["uploadlater"]."&uploadtohost=".urlencode($_POST['uploadtohost']) : "").
 			($_POST ['autoclose'] ? "&autoclose=1" : "").
-			(isset($_GET["audl"]) ? "&audl=doum" : "");
+			(isset($_GET["audl"]) ? "&audl=doum" : "") . $addon;
 		insert_location ( $loc );
 	}
 	
