c56d0fd71aed12d5ceb781fc34666db3f67b1bc2
05049pyj <05049PYJ@gmail.com>
2009-07-23 10:46:32 +0000

    Total extraction of English strings completed.

--- classes/options/pack.php
+++ classes/options/pack.php
@@ -4,25 +4,22 @@ function rl_pack() {
 	if (count ( $_GET ["files"] ) < 1) {
 		echo "Select at least one file.<br><br>";
 	} else {
-?>
-<form method="post"><input type="hidden" name="act" value="pack_go">
-<?php
+		echo('<form method="post"><input type="hidden" name="act" value="pack_go">');
 		echo count ( $_GET ["files"] ) . " file" . (count ( $_GET ["files"] ) > 1 ? "s" : "") . ":<br>";
 		
 		for($i = 0; $i < count ( $_GET ["files"] ); $i ++) {
 			$file = $list [$_GET ["files"] [$i]];
-			?>
-                                <input type="hidden" name="files[]"
-			value="<?php echo $_GET ["files"] [$i]; ?>"> <b><?php echo basename ( $file ["name"] ); ?></b><?php echo $i == count ( $_GET ["files"] ) - 1 ? "." : ",&nbsp;"; }?><br>
-		<br>
+?>
+<input type="hidden" name="files[]" value="<?php echo $_GET ["files"] [$i]; ?>"> <b><?php echo basename ( $file ["name"] ); ?></b><?php echo $i == count ( $_GET ["files"] ) - 1 ? "." : ",&nbsp;"; }?><br />
+		<br />
 		<table align="center">
 			<tr>
-				<td>Archive Name:&nbsp;<input type="text" name="arc_name"
+				<td><?php echo lang(195); ?>:&nbsp;<input type="text" name="arc_name"
 					size="30"></td>
 				<td><input type="submit" value="Pack"></td>
 			</tr>
 			<tr>
-				<td>Save To:&nbsp;<input type="text" name="path" size="30"
+				<td><?php echo lang(40); ?>:&nbsp;<input type="text" name="path" size="30"
 					value="<?php echo ($_COOKIE ["path"] ? $_COOKIE ["path"] : (strstr ( ROOT_DIR, "\\" ) ? addslashes ( dirname ( __FILE__ ) ) : dirname ( __FILE__ ))); ?>">
 				</td>
 			</tr>
@@ -44,19 +41,20 @@ function pack_go() {
 	global $list;
 	$smthExists = true;
 	if (count ( $_GET ["files"] ) < 1) {
-		echo "Select at least one file.<br><br>";
+		echo lang(138)."<br /><br />";
 		break;
 	}
 	
 	$arc_name = $_GET ["arc_name"];
 	if (! $arc_name) {
-		echo "Please enter an archive name!<br><br>";
+		echo lang(196)."<br /><br />";
 		break;
 	}
 	;
 	
 	if (file_exists ( $arc_name )) {
-		echo "File <b>" . $arc_name . "</b> already exists!<br><br>";
+		printf(lang(),$arc_name);
+		echo "<br /><br />";
 		break;
 	}
 	
@@ -65,11 +63,12 @@ function pack_go() {
 		if (file_exists ( $file ["name"] )) {
 			$v_list [] = $file ["name"];
 		} else {
-			echo "File <b>" . $file ["name"] . "</b> not found!<br><br>";
+			printf(lang(145),$file['name']);
+			echo "<br /><br />";
 		}
 	}
 	if (! $v_list) {
-		echo "An error occured!<br><br>";
+		echo lang(137)."<br /><br />";
 		break;
 	}
 	$arc_name = $path . '/' . $arc_name;
@@ -80,18 +79,19 @@ function pack_go() {
 	$tar = new Archive_Tar ( $arc_name );
 	$tar->create ( $v_list, $arc_method );
 	if (! file_exists ( $arc_name )) {
-		echo "Error! Archive not created.<br><br>";
+		echo lang(197)."<br /><br />";
 		break;
 	}
 	
 	if (count ( $v_list = $tar->listContent () ) > 0) {
-		echo "File";
-		echo count ( $v_list ) > 1 ? "s" : "";
-		echo "<br>";
+		echo lang(104);
+		echo "<br />";
 		for($i = 0; $i < sizeof ( $v_list ); $i ++) {
-			echo "File " . $v_list [$i] ["filename"] . " was packed <br>";
+			printf(lang(198),$v_list[$i]['filename']);
+			echo " <br />";
 		}
-		echo "Packed in archive <b>$arc_name</b><br>";
+		printf(lang(199),$arc_name);
+		echo "<br />";
 		
 		$stmp = strtolower ( $arc_name );
 		if (strrchr ( $stmp, "tar.gz" ) + 5 == strlen ( $stmp )) {
@@ -108,10 +108,10 @@ function pack_go() {
 		$time = str_replace ( "0.", $time [1], $time [0] );
 		$list [$time] = array ("name" => $arc_name, "size" => bytesToKbOrMbOrGb ( filesize ( $arc_name ) ), "date" => $time, "link" => "", "comment" => "archive " . $arc_method );
 	} else {
-		echo "Error! Archive is Empty.<br><br>";
+		echo lang(200)."<br /><br />";
 	}
 	if (! updateListInFile ( $list )) {
-		echo "Couldn't Update!<br><br>";
+		echo lang(9)."<br /><br />";
 	}
 }
 ?>
