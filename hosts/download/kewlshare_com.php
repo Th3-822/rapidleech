ac73bee12687334543ba372076ca4b17822caa72
kaoxiss <kaoxiss@gmail.com>
2009-09-04 11:01:11 +0000

    [FIX] kewlshare download plugin

--- hosts/download/kewlshare_com.php
+++ hosts/download/kewlshare_com.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('RAPIDLEECH'))
   {
   require_once("index.html");
@@ -20,9 +21,25 @@ if (!defined('RAPIDLEECH'))
 	is_page($page);
 	is_present($page,"increase your limit","Your Current Download Limit Per 1 hour is over. Please Upgrade your account to increase your limit. Thank You",0);
 	unset ($post);
-	insert_timer(2, "<b>Timer :</b>");
+	insert_timer(15, "<b>Timer :</b>");
 	$nL=cut_str($page, 'nL" value="', '"');
-	if(!$nL){html_error("download data is not found");}
+	if(!$nL){
+	
+	preg_match('/Location:.+?\\r/i', $page, $loca);
+    $redir = rtrim($loca[0]);
+    preg_match('/http:.+/i', $redir, $loca);
+	if (!$loca[0]){
+	html_error("download data is not found");}else{
+	$Url=parse_url($loca[0]);
+		$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"], $Referer, $cookie, 0, 0, $_GET["proxy"],$pauth);
+	    is_page($page);
+	
+	preg_match('/We had a request[^<]+/i', $page, $errmsg);
+    if ($errmsg[0]){
+	html_error($errmsg[0]);}else{
+        html_error("Wait for your turn");}
+    }
+    }
 	$cL=cut_str($page, 'cL" value="', '"');
 	$post["nL"]=$nL;
 	$post["cL"]=$cL;
@@ -30,14 +47,14 @@ if (!defined('RAPIDLEECH'))
 	$post['x'] = rand(1,140);
     $post['y'] = rand(1,20);
 	$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"], $Referer, $cookie, $post, 0, $_GET["proxy"],$pauth);
-	preg_match('/http:\/\/.+kewlshare\.com\/dl\/[^\'"]+/i', $page, $down);   
-    $Url=parse_url($down[0]);
+	preg_match_all('/http:\/\/.+kewlshare\.com\/dl\/[^\'"]+/i', $page, $down);   
+    $Url=parse_url($down[0][1]);
     $FileName = !$FileName ? basename($Url["path"]) : $FileName;
-    sleep(2);
     insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&port=".$Url["port"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&cookie=".urlencode($cookie)."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
 	
 /*************************\  
 written by kaox 13/06/2009
+fixed by kaox 04/09/2009
 \*************************/
 
 ?>
