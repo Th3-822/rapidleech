6852daf49f94d9faa078095826e1b201342441f3
kaoxiss <kaoxiss@0f90527e-9506-11de-8d2f-ebe662690e28>
2009-09-04 20:12:18 +0000

    [UPDATE] uploading.com download plugin

--- trunk/hosts/download/uploading_com.php
+++ trunk/hosts/download/uploading_com.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('RAPIDLEECH'))
 {
 	require_once("index.html");
@@ -11,95 +12,94 @@ $uploading_password="";  // password
 if (!uploading_username || !$uploading_password){
 
 $page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), 0, 0, 0, 0, $_GET["proxy"],$pauth);
-//file_put_contents("uploading_1.txt", $page);
-is_page($page);
-is_present($page, "Sorry, the file requested by you does not exist on our servers.");
+$fileID=cut_str($page,'file_id" value="','"');
 
 $cookie = GetCookies($page);
+$temp = cut_str($page,'class="file_download"','</div>');
+$FileName = trim(cut_str($temp,'<h2>','</h2>'));
 
+is_page($page);
+is_present($page, "Sorry, the file requested by you does not exist on our servers.");
 $post = Array();
-$post["free"] = 1;
-$cookie = 'redirect=1';
+$post["action"] = "second_page";
+$post["file_id"] = $fileID;
 $page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
-//file_put_contents("uploading_2.txt", $page);
-is_page($page);
-while(strpos($page,"Your IP address")){
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
-//file_put_contents("uploading_2.txt", $page);
-is_page($page);
-$cc++; 
-insert_timer(10,$cc." attempt to bypass message "."- Your IP address is already downloading a file."  );
-if ($cc>=8) {html_error("Not succes the server still report - Your IP address is already downloading a file. Please wait until the download is completed. wait for more minutes and reattempt",0);
-}    
 
+is_page($page);
+
+$tid=str_replace(".","12",microtime(true));
+$sUrl="http://uploading.com/files/get/?JsHttpRequest=".$tid."-xml";
+$Url=parse_url($sUrl);
+
+unset($post);
+$post["file_id"]=$fileID;
+$post["action"]="step_1";
+$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
+$wait=cut_str($page,'answer": "','"');
+if ($wait<0) {html_error("Please wait for some minute and reattempt",0);
 }
-/*
-is_present($page,"Your IP address","Your IP address is already downloading a file. Please wait until the download is completed.");
-*/
-$countDown = trim(cut_str($page, "var c2168=", ";"));
-if ($countDown==""){$countDown = 91;}
-$FileName = substr(basename($Url["path"]), 0, -5);
-$post = Array();
-$post["free"] = 1;
-$post["x"] = 1;
-$coo = "";
-$temp = "";
-preg_match_all('/Set-Cookie: (.*);/U',$page,$temp);
-$cookie = $temp[1];
-$cookie = implode(';',$cookie);
-insert_timer($countDown,"Wait for your turn");
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
-$loc = "";
-preg_match('/ocation: (.*)/',$page,$loc);
-$Href = $loc[1];
-$Url = parse_url($Href);
-$FileName = !$FileName ? basename($Url["path"]) : $FileName;
+insert_timer($wait);
+$tid=str_replace(".","12",microtime(true));
+$sUrl="http://uploading.com/files/get/?JsHttpRequest=".$tid."-xml";
+$Url=parse_url($sUrl);
+unset($post);
+$post["file_id"]=$fileID;
+$post["action"]="step_2";
+
+$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie,$post, 0, $_GET["proxy"],$pauth);
+$dUrl=str_replace("\\","",cut_str($page,'redirect": "','"'));
+
+if ($dUrl=="") {html_error("Download url error , Please wait for some minute and reattempt",0);
+}
+$Url=parse_url($dUrl);
 
 insert_location("$PHP_SELF?cookie=".urlencode($cookie)."&filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
+
 }else{
-$in=parse_url("http://uploading.com/login/");
+$in=parse_url("http://uploading.com/general/login_form/");
 $post=array();
-$post["log_ref"]="";
-$post["login"]=$uploading_username;
-$post["pwd"]=$uploading_password;
+$post["email"]=$uploading_username;
+$post["password"]=$uploading_password;
 $page = geturl($in["host"], $in["port"] ? $in["port"] : 80, $in["path"].($in["query"] ? "?".$in["query"] : ""), "http://uploading.com/login/", 0, $post, 0, $_GET["proxy"],$pauth);
-$cook=biscottiDiKaox($page);
+$cookie=GetCookies($page);
 $in=parse_url("http://uploading.com/");
-$page = geturl($in["host"], $in["port"] ? $in["port"] : 80, $in["path"].($in["query"] ? "?".$in["query"] : ""), "http://uploading.com/login/", $cook, 0, 0, $_GET["proxy"],$pauth);
-if(!strpos($page,"Logout")){
+$page = geturl($in["host"], $in["port"] ? $in["port"] : 80, $in["path"].($in["query"] ? "?".$in["query"] : ""), "http://uploading.com/login/", $cookie, 0, 0, $_GET["proxy"],$pauth);
+if(!strpos($page,$uploading_username)){
 html_error("Login Failed , Bad username/password combination.",0);
 }
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $referer, $cook, 0, 0, $_GET["proxy"],$pauth);
-$cook=biscottiDiKaox($page);
+
+$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $referer, $cookie, 0, 0, $_GET["proxy"],$pauth);
+$fileID=cut_str($page,"file_id: '","'");
 $tmp = basename($Url["path"]);
 $FileName=str_replace(".html","",$tmp);
+
+$tid=str_replace(".","12",microtime(true));
+$sUrl="http://uploading.com/files/get/?JsHttpRequest=".$tid."-xml";
+$Url=parse_url($sUrl);
+
 unset($post);
-$post["premium"]="1";
-$post["x"]="1";
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $referer, $cook, $post, 0, $_GET["proxy"],$pauth);
-is_present($page,"Location: /premium/","You are Free members,please insert a Premium account");
-preg_match('/Location:.+?\\r/i', $page, $loca);
-$redir = rtrim($loca[0]);
-preg_match('/http:.*/i', $redir, $loca);
-$Url=parse_url($loca[0]);
+$post["file_id"]=$fileID;
+$post["action"]="step_1";
+$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
+
+$tid=str_replace(".","12",microtime(true));
+$sUrl="http://uploading.com/files/get/?JsHttpRequest=".$tid."-xml";
+$Url=parse_url($sUrl);
+
+unset($post);
+$post["file_id"]=$fileID;
+$post["action"]="step_2";
+$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
+
+$dUrl=str_replace("\\","",cut_str($page,'redirect": "','"'));
+if ($dUrl=="") {html_error("Download url error , Please reattempt",0);
+}
+$Url=parse_url($dUrl);
 
 insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK)."&cookie=".urlencode($cookie).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
 }
-function biscottiDiKaox($content)
- {
- preg_match_all("/Set-Cookie: (.*)\n/",$content,$matches);
- foreach ($matches[1] as $coll) {
- $bis0=split(";",$coll);
- $bis1=$bis0[0]."; ";
- if(strpos($bis1,"=deleted") || strpos($bis1,"=;")) {
- }else{
- $bis2=split("=",$bis1);
-    $cek=" ".$bis2[0]."=";
-if  (substr_count($bis,$cek)>0)
-{$patrn=" ".$bis2[0]."=[^ ]+";
-$bis=preg_replace("/$patrn/"," ".$bis1,$bis);     
-} else {$bis.=$bis1;}}}  
-$bis=str_replace("  "," ",$bis);     
-return rtrim($bis);}
-// written by kaox 24/05/2009
+/*************************\  
+WRITTEN by kaox 24/05/2009
+UPDATE by kaox 04/09/2009
+\*************************/
 ?>
