438a6ad62cb1b6b9180f0ce511528e6b14c8c66c
kaoxiss <kaoxiss@0f90527e-9506-11de-8d2f-ebe662690e28>
2009-09-06 15:55:40 +0000

    [FIX] depositfiles.com upload plugin

--- trunk/hosts/upload/depositfiles.com.index.php
+++ trunk/hosts/upload/depositfiles.com.index.php
@@ -1,5 +1,5 @@
 <?php 
-$upload_services[] = "depositfiles.com_premium";
-$max_file_size["depositfiles.com_premium"] = 2000;
-$page_upload["depositfiles.com_premium"] = "depositfiles.com_premium.php";  
+$upload_services[] = "depositfiles.com";
+$max_file_size["depositfiles.com"] = 2000;
+$page_upload["depositfiles.com"] = "depositfiles.com.php";  
 ?>
