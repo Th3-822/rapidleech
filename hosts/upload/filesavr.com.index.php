957cbdc56a47eb89cbe2cf19274447cf995b504a
rl.baking <rl.baking@gmail.com>
2009-09-14 14:45:29 +0000

    [PLUGIN] Added some newly made plugins [FIX] MegaFTP member upload plugin [FIX] Multiup member upload plugin

---
+++ hosts/upload/filesavr.com.index.php
@@ -0,0 +1,5 @@
+<?php
+$upload_services[]="filesavr.com";
+$max_file_size["filesavr.com"]=10000;
+$page_upload["filesavr.com"] = "filesavr.com.php";  
+?>
---
+++ hosts/upload/uploadmirrors.com.index.php
@@ -0,0 +1,5 @@
+<?php
+$upload_services[]="uploadmirrors.com";
+$max_file_size["uploadmirrors.com"]=100;
+$page_upload["uploadmirrors.com"] = "uploadmirrors.com.php";  
+?>
