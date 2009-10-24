554f765378cded8b8f5d03126ca2050509c9f5a7
05049PYJ <05049PYJ@gmail.com>
2009-10-24 07:02:14 +0000

    [CHANGE] Link checker is now more flexible. And btw, its a long time since I committed!

--- configs/config.php
+++ configs/config.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('RAPIDLEECH'))
-  {
-  require_once("index.html");
-  exit;
-  }
+{
+	require_once("index.html");
+	exit;
+}
 
 $premium_acc = array();
 ### Remove '//' from the beginning and enter your username and password for enable premium account ###
@@ -57,7 +57,7 @@ $ajax_refresh = true;
 //$mu_cookie_user_value = '';  // like: b658b369856766f621ca292fac113a5c, that contains username&pass of premium account and can be shared to others, because it's an encrypted value.
 
 ### Imageshack Torrent Account ###
-//$imageshack_acc = array('user' => 'your username', 'pass' => 'your password'); 
+//$imageshack_acc = array('user' => 'your username', 'pass' => 'your password');
 
 ###Auto Download Premium Account###
 //$premium_acc["au_dl"] = array('user' => 'your username', 'pass' => 'your password'); # Remove '//' from the beginning and enter your username and password for rapidshare.de premium account
@@ -96,4 +96,44 @@ $options["disable_unzip"] = false; // Disable Unzip action // false = allow
 $options["disable_upload"] = false; // Disable Upload action // false = allow
 $options["disable_zip"] = false; // Disable ZIP action // false = allow
 $options['disable_list'] = false; // Disable list links // false = allow
+
+### Here is a list of checkable links ###
+// Only update here if needed so we can synchronize it with templates
+$sites = array(
+	array('name' => 'Rapidshare.com', 'link' => 'rapidshare\.com/files/', 'regex' => '(FILE DOWNLOAD|This file is larger than 200 Megabyte)'),
+	array('name' => 'Megashares.com', 'link' => 'megashares\.com/\?d01=', 'regex' => 'Click here to download'),
+	array('name' => 'Megaupload.com', 'link' => 'megaupload\.com/([a-z]{2}/)?\?d=', 'regex' => '(Filename:)|(All download slots assigned to your country)'),
+	array('name' => 'Filefactory.com', 'link' => 'filefactory\.com/file/', 'regex' => '(download link)|(Please try again later)'),
+	array('name' => 'Rapidshare.de', 'link' => 'rapidshare\.de/files/', 'regex' => 'You want to download'),
+	array('name' => 'Mediafire.com', 'link' => 'mediafire\.com/(download\.php)?\?', 'regex' => 'You requested'),
+	array('name' => 'Netload.in', 'link' => 'netload\.in/datei[0-9a-z]{32}/', 'regex' => 'download_load'),
+	array('name' => 'Depositfiles.com', 'link' => 'depositfiles\.com/([a-z]{2}/)?files/', 'regex' => 'File Name', 'pattern' => '@(com\/files\/)|(com\/[a-z]{2}\/files\/)@i', 'replace' => '"com/en/files/'),
+	array('name' => 'Sendspace.com', 'link' => 'sendspace\.com/file/', 'regex' => 'The download link is located below.'),
+	array('name' => 'iFile.it', 'link' => 'ifile\.it/', 'Request Ticket'),
+	array('name' => 'USAUpload.net', 'link' => 'usaupload\.net/d/', 'regex' => 'This is the download page for file'),
+	array('name' => 'Badango.com', 'link' => 'badongo\.com/([a-z]{2}/)?(file)|(vid)/', 'regex' => 'fileBoxMenu'),
+	array('name' => 'Uploading.com', 'link' => 'uploading\.com/files/', 'regex' => 'Download file'),
+	array('name' => 'Savefile.com', 'link' => 'savefile\.com/files/', 'regex' => 'link to this file'),
+	array('name' => 'Cocoshare.cc', 'link' => 'cocoshare\.cc/[0-9]+/', 'regex' => 'Filesize:'),
+	array('name' => 'Axifile.com', 'link' => 'axifile\.com/?', 'regex' => 'You have request', 'pattern' => '@com\?@i', 'com/?'),
+	array('name' => 'Turboupload.com', 'link' => '(d\.turboupload\.com/)|(turboupload.com/download/)', 'regex' => '(Please wait while we prepare your file.)|(You have requested the file)'),
+	array('name' => 'Files.to', 'link' => 'files\.to/get/', 'regex' => 'You requested the following file'),
+	array('name' => 'Gigasize.com', 'link' => 'gigasize\.com/get\.php\?d=', 'regex' => 'Downloaded'),
+	array('name' => 'Ziddu.com', 'link' => 'ziddu\.com/', 'regex' => 'Download Link'),
+	array('name' => 'ZShare.net', 'link' => 'zshare\.net/(download|audio|video)/', 'regex' => 'Last Download'),
+	array('name' => 'Uploaded.to', 'link' => 'uploaded\.to/(\?id=|file/)', 'regex' => 'Filename:'),
+	array('name' => 'Filefront.com', 'link' => 'filefront\.com/', 'regex' => 'http://static4.filefront.com/ffv6/graphics/b_download_still.gif'),
+	array('name' => 'UploadPalace.com', 'link' => 'uploadpalace\.com/[a-zA-Z]{2}/file/[0-9]+/', 'regex' => 'Filename:'),
+	array('name' => 'SpeedyShare.com', 'link' => 'speedyshare\.com/[0-9]+\.html', '/data/'),
+	array('name' => 'Momupload.com', 'link' => 'momupload\.com/files/', 'regex' => 'You want to download the file'),
+	array('name' => 'Rnbload.com', 'link' => 'rnbload\.com/file/', 'regex' => 'Filename:'),
+	array('name' => 'iFolder.ru', 'link' => 'ifolder\.ru/[0-9]+', 'regex' => 'ints_code'),
+	array('name' => 'ADrive.com', 'link' => 'adrive\.com/public/', 'regex' => 'view'),
+	array('name' => 'Easy-share.com', 'link' => 'easy-share\.com', 'regex' => 'file url:'),
+	array('name' => 'BitRoad.net', 'link' => 'bitroad\.net/download/[0-9a-z]+/', 'regex' => 'File:'),
+	array('name' => 'Megarotic.com', 'link' => 'megarotic\.com/([a-z]{2}/)?\?d=', 'regex' => '(Filename:)|(All download slots assigned to your country'),
+	array('name' => 'Egoshare.com', 'link' => 'egoshare\.com', 'regex' => 'You have requested'),
+	array('name' => 'Flyupload.com', 'link' => 'flyupload\.flyupload.com/get\?fid', 'regex' => 'Download Now'),
+	array('name' => 'Megashare.com', 'link' => 'megashare\.com/[0-9]+', 'regex' => 'Free'),
+);
 ?>
