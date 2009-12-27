53153b21d0a1d13b068b55177261f4e2f7c725a1
rl.baking <rl.baking@gmail.com>
2009-12-27 19:43:55 +0000

    [ADD][PLUGIN] Added some newly made upload plugins

---
+++ hosts/upload/divxden.com_free.index.php
@@ -0,0 +1,5 @@
+<?php
+$upload_services[]="divxden.com_free";
+$max_file_size["divxden.com_free"]=1000;
+$page_upload["divxden.com_free"] = "divxden.com_free.php";  
+?>
---
+++ hosts/upload/missupload.com_free.index.php
@@ -0,0 +1,5 @@
+<?php
+$upload_services[]="missupload.com_free";
+$max_file_size["missupload.com_free"]=50;
+$page_upload["missupload.com_free"] = "missupload.com_free.php";  
+?>
