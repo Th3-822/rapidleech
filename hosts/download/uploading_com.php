d486ced1032400306d0e9e48b2e139bdea5efd08
malhotra1raj <malhotra1raj@gmail.com>
2010-01-19 19:25:37 +0000

    [Fixed][Download][PLUGIN] uploading.com download plug-in

--- hosts/download/uploading_com.php
+++ hosts/download/uploading_com.php
@@ -52,7 +52,10 @@ insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]
 }else
 {
 $page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), 0, 0, 0, 0, $_GET["proxy"],$pauth);
-$fileID=cut_str($page,'file_id" value="','"');
+$fileID = trim ( cut_str($page,'file_id" value="','"') );
+$action = trim ( cut_str($page,'action" value="','"') );
+$Href_1 = trim ( cut_str($page,'form action="http://uploading.com/files','"') );
+$Href_1 = "http://uploading.com/files".$Href_1;
 
 $cookie = GetCookies($page);
 $temp = cut_str($page,'<div class="c_1','</div>');
@@ -60,14 +63,26 @@ $FileName = trim(cut_str($temp,'<h2>','</h2>'));
 
 is_page($page);
 is_present($page, "Sorry, the file requested by you does not exist on our servers.");
+is_present($page, "We are sorry, the file was removed either by its owner or due to the complaint received" );
+if (stristr ( $page, "Sorry, you can download only one file per" ))
+{
+	$minutes = trim ( cut_str ( $page, "Sorry, you can download only one file per ", " minutes." ) );
+	if ($minutes)
+	{
+		html_error ( "Download limit exceeded. Sorry, you can download only one file per <font color=black><span id='waitTime'>$minutes</span></font> minutes.Please try again later or acquire a premium membership.", 0 ); 
+	}
+	else
+	{
+		html_error ( "Download limit exceeded. Please try again later or acquire a premium membership.", 0 ); 
+	}
+}
 
-
-$code=explode("/",$Url["path"]);
-$Url["path"]="/files/get/".$code[count($code)-2]."/";
-
+$Url = parse_url( $Href_1 );
+//$code=explode("/",$Url["path"]);
+//$Url["path"]="/files/get/".$code[count($code)-2]."/";
 
 $post = Array();
-$post["action"] = "second_page";
+$post["action"] = $action;
 $post["file_id"] = $fileID;
 $page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
 
@@ -75,19 +90,26 @@ is_page($page);
 
 is_present($page, "Requested file not found");
 
+/*
 preg_match_all('/start_timer\((.*)\)/i', $page,$tm);
-$waitt=$tm[1][1];
-insert_timer($waitt+10); 
+//$waitt=$tm[1][1];
+//insert_timer($waitt+10); 
+*/
+
+preg_match_all('/start_timer\((\d+)\)/i', $page,$tm);
+$count = trim ( $tm[1][0] );
+
+insert_timer( $count, "Waiting link timelock", "", true );
 
 $tid=str_replace(".","12",microtime(true));
 $sUrl="http://uploading.com/files/get/?JsHttpRequest=".$tid."-xml";
 $Url=parse_url($sUrl);
 
- unset($post);
+unset($post);
 $post["file_id"]=$fileID;
 $post["action"]="get_link";
 $post["pass"]="";
-    $page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
+$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $LINK, $cookie, $post, 0, $_GET["proxy"],$pauth);
 
 $dUrl=str_replace("\\","",cut_str($page,'answer": { "link": "','"'));
 
@@ -95,11 +117,13 @@ if ($dUrl=="") {html_error("Download url error , Please wait for some minute and
 }
 $Url=parse_url($dUrl);
 
-insert_location("$PHP_SELF?cookie=".urlencode($cookie)."&filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
+$loc = "$PHP_SELF?cookie=".urlencode($cookie)."&filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""); 
+insert_location( $loc );
 
 }
 /**************************************************\  
 WRITTEN by kaox 24-may-2009
 UPDATE by kaox  29-nov-2009
+UPDATE by rajmalhotra  20 Jan 2010
 \**************************************************/
 ?>
