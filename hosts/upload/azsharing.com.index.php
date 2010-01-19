16c700b8564740b66aea2debc4ab0d54a1a19aac
malhotra1raj <malhotra1raj@gmail.com>
2010-01-19 20:55:58 +0000

    [Updated][Upload][PLUGIN] updated size limit on some host

--- hosts/upload/azsharing.com.index.php
+++ hosts/upload/azsharing.com.index.php
@@ -1,5 +1,5 @@
 <?php
 $upload_services[]="azsharing.com";
-$max_file_size["azsharing.com"]=100;
+$max_file_size["azsharing.com"]=200;
 $page_upload["azsharing.com"] = "azsharing.com.php";  
 ?>
--- hosts/upload/easy-share.com.index.php
+++ hosts/upload/easy-share.com.index.php
@@ -1,5 +1,5 @@
 <?php 
 $upload_services[] = "easy-share.com";
-$max_file_size["easy-share.com"] = 100;
+$max_file_size["easy-share.com"] = 200;
 $page_upload["easy-share.com"] = "easy-share.com.php";
 ?>
