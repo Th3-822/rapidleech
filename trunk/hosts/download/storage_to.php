cb4cf4571ecc32686629f0b1678fe0baed384f43
malhotra1raj <malhotra1raj@0f90527e-9506-11de-8d2f-ebe662690e28>
2010-01-24 06:58:06 +0000

    [Update][PLUGIN] storage.to download plug-in => Added support for downloading from premium account.

--- trunk/hosts/download/storage_to.php
+++ trunk/hosts/download/storage_to.php
@@ -10,17 +10,55 @@ class storage_to extends DownloadClass
 	public function Download( $link )
 	{
 		global $premium_acc;
-		$this->DownloadFree($link);
+		if (($_REQUEST ["premium_acc"] == "on" && $_REQUEST ["premium_user"] && $_REQUEST ["premium_pass"]) ||
+			($_REQUEST ["premium_acc"] == "on" && $premium_acc ["storage"] ["user"] && $premium_acc ["storage"] ["pass"]))
+		{
+			$this->DownloadPremium($link);
+		}
+		else
+		{
+			$this->DownloadFree($link);
+		}
 	}
 
 	private function DownloadFree($link)
 	{
-		$Referer = $link;
+		$this->DownloadLink( $link );
+	}
+	
+	private function DownloadPremium($link)
+	{
+		global $premium_acc, $Referer;
+		
+		$loginUrl = "http://storage.to/login";
+		$usrEmail = "";
+
+		$post=array();
+		$usrEmail = $_REQUEST ["premium_user"] ? $_REQUEST ["premium_user"] : $premium_acc ["storage"] ["user"];
+		$post["email"] = $usrEmail;
+		$post["password"] = $_REQUEST ["premium_pass"] ? $_REQUEST ["premium_pass"] : $premium_acc ["storage"] ["pass"];
+		$post[""] = "Login";
+		$page = $this->GetPage( $loginUrl, 0, $post, $loginUrl );
+		
+		$cookie = GetCookies($page);
+		
+		if( !strpos( $page, "Login successful") )
+		{
+			html_error("Login Failed , Bad username/password combination.",0 );
+		}
+		
+		$this->DownloadLink( $link, $cookie );
+	}
+	
+	private function DownloadLink( $link, $cookie = 0 )
+	{
+		global $Referer;
+		
 		preg_match('%/get/(.+?)/%', $link, $id);
 		$id = $id[1];
 		$getlink = "http://www.storage.to/getlink/$id/";
 
-		$page = $this->GetPage( $getlink );
+		$page = $this->GetPage( $getlink, $cookie );
 		
 		preg_match('/Object\((.+?)\)/', $page, $json);
 		if (!$json[1])
@@ -35,9 +73,13 @@ class storage_to extends DownloadClass
 			html_error("Cannot decode Json string!",0);
 		}
 
-		if ($jsarray['state'] == "failed")
+		if ($jsarray['state'] == "failed" && $jsarray['linkid'] == "" )
 		{
-			html_error("The download failed. Please try again later",0);
+			html_error("File not found. Kindly check the link",0 );
+		}
+		else if ($jsarray['state'] == "failed")
+		{
+			html_error("The download failed. Please try again later",0 );
 		}
 		else if ($jsarray['state'] == "wait")
 		{
@@ -45,16 +87,21 @@ class storage_to extends DownloadClass
 		}
 		else if ($jsarray['state'] == "ok")
 		{
-			insert_timer( $jsarray['countdown'], "Waiting link timelock", "", true );
+			$count = $jsarray['countdown'];
+			if ( $count > 0 )
+			{
+				insert_timer( $count, "Waiting link timelock", "", true );
+			}
 		}
+						
+		$Href = $jsarray['link'];		
+		$FileName = basename( $Href );
 		
-		$FileName = basename($jsarray['link']);
-		
-		$Href = $jsarray['link'];
-		$this->RedirectDownload($Href,$FileName,0, 0,$Referer);
+		$this->RedirectDownload( $Href, $FileName, $cookie, 0, $Referer );
 		exit ();
 	}
 }
 
 // Created by rajmalhotra on 20 Jan 2010
+// Updated by rajmalhotra on 24 Jan 2010 -> Added premium account support.
 ?>
