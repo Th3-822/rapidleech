b53e5209c057f39c664dac4191d2b679370809dc
malhotra1raj <malhotra1raj@0f90527e-9506-11de-8d2f-ebe662690e28>
2010-01-31 17:09:45 +0000

    [Update][PLUGIN] uploaded.to download plug-in => Fixed downloading from premium account and converted in OOPs form.

--- trunk/hosts/download/uploaded_to.php
+++ trunk/hosts/download/uploaded_to.php
@@ -1,59 +1,90 @@
-<?php //szalinski 25-Aug-09
-if (!defined('RAPIDLEECH'))
+<?php
+if (! defined ( 'RAPIDLEECH' ))
 {
-	require_once("index.html");
-	exit;
+	require_once ("index.html");
+	exit ();
 }
 
-if (($_POST["premium_acc"] == "on" && $_REQUEST["premium_user"] && $_REQUEST["premium_pass"]) || ($_POST["premium_acc"] == "on" && $premium_acc["uploaded_to"]["user"] && $premium_acc["uploaded_to"]["pass"]))
+class uploaded_to extends DownloadClass 
 {
-	$Url = parse_url('http://uploaded.to');
-	$post = array();
-	$post["email"] = $_REQUEST["premium_user"] ? $_REQUEST["premium_user"] : $premium_acc["uploaded_to"]["user"];
-	$post["password"] = $_REQUEST["premium_pass"] ? $_REQUEST["premium_pass"] : $premium_acc["uploaded_to"]["pass"];
-	$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, "/login", 0, 0, $post, 0, $_GET["proxy"],$pauth);
-	is_page($page);
-	$cook = GetCookies($page);
-	$Url = parse_url($LINK);
-	if ($Url['host'] == 'uploaded.to')
+	public function Download($link) 
 	{
-		$LINK = str_replace(array('uploaded.to', 'file/'), array('ul.to', ''), $LINK);
-		$Url = parse_url($LINK);
+		global $premium_acc;
+		if ( ($_REQUEST ["premium_acc"] == "on" && $_REQUEST ["premium_user"] && $_REQUEST ["premium_pass"]) ||
+			($_REQUEST ["premium_acc"] == "on" && $premium_acc ["uploaded_to"] ["user"] && $premium_acc ["uploaded_to"] ["pass"] ) )
+		{
+			$this->DownloadPremium($link);
+		}
+		else
+		{
+			$this->DownloadFree($link);
+		}
 	}
-	$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), 0, $cook, 0, 0, $_GET["proxy"], $pauth);
-	is_page($page);
-	preg_match('%ocation: (.+)\r\n%', $page, $redir);
-	$Href = trim($redir[1]);
-	$Url = parse_url($Href);
-	$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), 0, $cook, 0, 0, $_GET["proxy"], $pauth);
-	is_page($page);
-	preg_match('%ocation: (.+)\r\n%', $page, $redir);
-	$Href = trim($redir[1]);
-	$Url = parse_url($Href);
-	$Url["path"] = str_replace('//', '/', $Url["path"]);
-	$FileName = !$FileName ? basename($Url["path"]) : $FileName;
-	insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&cookie=".urlencode($cook)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "").($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
-}
-else
-{
-	$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), 0, 0, 0, 0, $_GET["proxy"],$pauth);
-	//file_put_contents("uploaded_to_1.txt", $page);
-	is_page($page);
-	is_present($page, "Location: /?view=error_fileremoved", "File not found");
-	is_present($page, "Location: /?view=error_traffic_exceeded_free", "Download limit exceeded");
-	is_present($page, "http://images.uploaded.to/key.gif", "This file is password protected");
+	
+	private function DownloadFree( $link )
+	{
+		$page = $this->GetPage( $link );
+		
+		is_present($page, "Location: /?view=error_fileremoved", "File not found");
+		is_present($page, "Location: /?view=error_traffic_exceeded_free", "Download limit exceeded");
+		is_present($page, "http://images.uploaded.to/key.gif", "This file is password protected");
 
-	if (preg_match('/(http.+dl\?id=[0-9a-zA-Z]+)/', $page, $dllink))
-	{
-		$dlink = $dllink[1];
+		if (preg_match('/(http.+dl\?id=[0-9a-zA-Z]+)/', $page, $dllink))
+		{
+			$dlink = trim ( $dllink[1] );
+		}
+		else
+		{
+			html_error("Download link not found", 0);
+		}
+		
+		$FileName = "none";
+		
+		$this->RedirectDownload( $dlink, $FileName );
+		exit ();	
 	}
-	else
+	
+	private function DownloadPremium( $link )
 	{
-		html_error("Download link not found", 0);
+		global $premium_acc, $Referer;
+			
+		$post = array();
+		$post["email"] = $_REQUEST["premium_user"] ? $_REQUEST["premium_user"] : $premium_acc["uploaded_to"]["user"];
+		$post["password"] = $_REQUEST["premium_pass"] ? $_REQUEST["premium_pass"] : $premium_acc["uploaded_to"]["pass"];
+		
+		$page = $this->GetPage( 'http://uploaded.to/login', 0, $post );
+		
+		if( strpos($page,"Login failed") )
+		{
+			html_error("Login Failed , Bad username/password combination.",0);
+		}
+		
+		$cook = GetCookies($page);
+		
+		$page = $this->GetPage( $link, $cook );
+		
+		preg_match ( '/Location: (.*)/', $page, $redir );
+		$hr = $redir[1];
+		if ( !isset( $hr ) )
+		{
+			$Href = trim ( cut_str ( $page, 'download_form" method="post" action="', '">' ) );
+			$Href = "http://uploaded.to".$Href;
+					
+			$post = array();
+			$page = $this->GetPage( $Href, $cook, $post );
+		}
+				
+		preg_match ( '/Location: (.*)/', $page, $newredir );
+		$Href = trim ( $newredir [1] );
+		$Url = parse_url($Href);
+		$FileName = !$FileName ? basename($Url["path"]) : $FileName;
+		
+		$this->RedirectDownload( $Href, $FileName, $cook );
+		exit ();	
 	}
-	$Url = parse_url($dlink);
-	$FileName = "none";
+}	
 
-	insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "").($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
-}
+/**************************************************\  
+Fixed premium account support and Updated by rajmalhotra 31 Jan 2010
+\**************************************************/
 ?>
