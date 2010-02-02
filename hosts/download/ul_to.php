31fcc81d68373648291fd6182f0b9aafeca223cd
malhotra1raj <malhotra1raj@gmail.com>
2010-02-02 16:24:45 +0000

    [Update][PLUGIN] uploaded.to and ul.to download plug-in

--- hosts/download/ul_to.php
+++ hosts/download/ul_to.php
@@ -1,8 +1,19 @@
 <?php
-if (!defined('RAPIDLEECH'))
+if (! defined ( 'RAPIDLEECH' ))
 {
-	require_once("index.html");
-	exit;
+	require_once ("index.html");
+	exit ();
 }
-include_once('uploaded_to.php');
+
+	require_once (HOST_DIR . 'download/' . "uploaded_to.php");
+
+	$id = substr( $LINK, 13, 5 );
+	$link = "http://uploaded.to/file/".$id;
+		
+	$hostClass = new uploaded_to();
+	$hostClass->Download( $link );
+
+/**************************************************\  
+Updated by rajmalhotra 02 Feb 2010
+\**************************************************/
 ?>
