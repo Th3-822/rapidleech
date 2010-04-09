59ffbe6b48b229b6747a867bf2d2453757f79d8b
malhotra1raj <malhotra1raj@0f90527e-9506-11de-8d2f-ebe662690e28>
2010-04-09 19:23:04 +0000

    [Fixed][PLUGIN] Fixed 2shared.com download plugin => Fix Reloading to main page when link does not exists.

--- trunk/hosts/download/2shared_com.php
+++ trunk/hosts/download/2shared_com.php
@@ -4,24 +4,36 @@ if (!defined('RAPIDLEECH')) {
 	exit;
 }
 
-class d2shared_com extends DownloadClass {
-	public function Download($link) {
+class d2shared_com extends DownloadClass 
+{
+	public function Download( $link ) 
+	{
 		$page = $this->GetPage($link);
+		is_present( $page, "file link that you requested is not valid", "The file link that you requested is not valid. Please contact link publisher or try to make a search", "0" );
+		
 		$this->getCountDown($page);
 		$loc = "";
 		$FileName = "";
+		
 		// Retrieve location
 		preg_match ( '/location = "(.*)";/', $page, $loc );
 		$Href = $loc [1];
 		$Url = parse_url ( $Href );
 		$FileName = ! $FileName ? basename ( $Url ["path"] ) : $FileName;
-		$this->RedirectDownload($Href, $FileName);
+		$this->RedirectDownload( $Href, $FileName );
 	}
-	private function getCountDown($page) {
-		if (preg_match ( '/var c = ([0-9])*;/', $page, $count )) {
+	
+	private function getCountDown($page) 
+	{
+		if (preg_match ( '/var c = ([0-9])*;/', $page, $count ) ) 
+		{
 			$countDown = $count [1];
 			$this->CountDown($countDown);
 		}
 	}
 }
+
+/********************************************************
+Fixed by Raj Malhotra on 10 April 2010 => Fix Reloading to main page when link does not exists.
+*********************************************************/
 ?>
