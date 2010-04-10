adf969814322f1b191077a6bf7289be6e1cf89ad
kaoxiss <kaoxiss@0f90527e-9506-11de-8d2f-ebe662690e28>
2010-04-10 21:49:12 +0000

    [FIX]przeklej upload plugin

--- trunk/hosts/upload/przeklej.pl.php
+++ trunk/hosts/upload/przeklej.pl.php
@@ -1,66 +1,103 @@
+<?php 
+####### Free Account Info. ###########
+$przeklej_username=""; //  Set you username
+$przeklej_password=""; //  Set your password
+##############################
+
+$not_done=true;
+$continue_up=false;
+if ($przeklej_username & $przeklej_password){
+	$_REQUEST['my_login'] = $przeklej_username;
+	$_REQUEST['my_pass'] = $przeklej_password;
+	$_REQUEST['action'] = "FORM";
+	echo "<b><center>Use Default login/pass.</center></b>\n";
+}
+if ($_REQUEST['action'] == "FORM")  
+    $continue_up=true;
+else{
+?>
+<table border=0 style="width:270px;" cellspacing=0 align=center>
+<form method=post>
+<input type=hidden name=action value='FORM' />
+<tr><td nowrap>&nbsp;Username*<td>&nbsp;<input type=text name=my_login value='' style="width:160px;" />&nbsp;</tr>
+<tr><td nowrap>&nbsp;Password*<td>&nbsp;<input type=password name=my_pass value='' style="width:160px;" />&nbsp;</tr>
+<tr><td colspan=2 align=center><input type=submit value='Upload' /></tr>
+<tr><td colspan=2 align=center><small>*You can set it as default in <b><?php echo $page_upload["przeklej.pl"]; ?></b></small></tr>
+</table>
+</form>
+
+<?php
+	}
+
+if ($continue_up)
+	{
+		$not_done=false;
+?>
 <table width=600 align=center>
 </td></tr>
 <tr><td align=center>
 <div id=info width=100% align=center>Retrive upload ID</div>
-<?php
-			$url = "http://www.przeklej.pl";
-			$Url = parse_url($url);
-			$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), 0, 0, 0, 0, $_GET["proxy"],$pauth);
+<?			
+            $usr=$_REQUEST['my_login'];
+            $pass=$_REQUEST['my_pass'];
+            $referrer="http://www.przeklej.pl/"; 
+            $Url = parse_url("http://www.przeklej.pl/loguj");
+			$post['login[login]'] = $usr;
+			$post['login[pass]'] = $pass;
+            $page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $referrer, 0, $post, 0, $_GET["proxy"],$pauth);
 			is_page($page);
-			
-			preg_match_all('/Set-Cookie: (.*);/U',$page,$temp);
-			$cookie = $temp[1];
-			$cookies = implode(';',$cookie);
-			
-			$token=cut_str($page,'hidden" name="token" value="','"');
-			
-			$post["Filename"] = $lname;
-			$post['token'] = $token;
-			$post["newUpload"] = "1";
-			$post['Upload'] = "Submit Query";
-						
-?>
+			$cookie =GetCookies($page);
+			$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $referrer, $cookie, 0, 0, $_GET["proxy"],$pauth);
+			is_page($page);
+            is_notpresent($page,$usr."</strong>!","Not logged in. Check your login details in przeklej.pl.php");	
+            $url_action="http://www.przeklej.pl/simple_upload" ;
+			$Url = parse_url($url_action);
+			$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $referrer, $cookie, 0, 0, $_GET["proxy"],$pauth);
+			is_page($page);
+			$tok=cut_str($page, 'name="token" value="', '"');
+			$fpost['token'] = $tok;
+	?>
 <script>document.getElementById('info').style.display='none';</script>
-<?php 		
-			$url = parse_url('http://www.przeklej.pl/dodaj_plik');
-			$upagent = "Shockwave Flash";
-			$upfiles = upfile($url["host"], defport($url), $url["path"].($url["query"] ? "?".$url["query"] : ""), 0, $cookies, $post, $lfile, $lname, "plik[plik]",0,$upagent);
+
+		<table width=600 align=center>
+			</td>
+			</tr>
+			<tr>
+				<td align=center>
+<?php		
+			$url = parse_url("http://www.przeklej.pl/dodaj_plik_form");
+			$upagent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.1) Gecko/2008070208 Firefox/3.0.1";
+			$upfiles = upfile($url["host"],$url["port"] ? $url["port"] : 80, $url["path"].($url["query"] ? "?".$url["query"] : ""),$referrer, $cookie, $fpost, $lfile, $lname, "plik[plik]");
+
 ?>
 <script>document.getElementById('progressblock').style.display='none';</script>
 <?php 	
-			is_page($upfiles);
-			
-			unset ($cookies);
-			preg_match_all('/Set-Cookie: (.*);/U',$page,$temp);
-			$cookie = $temp[1];
-			$cookies = implode(';',$cookie);
-			
-			$fileidsup = cut_str($upfiles,'f,',',');
-			$filetokens = "tok%3A".cut_str($upfiles,'tok:','\n');
-			
-			$upost['fileids[fileids]'] = $fileidsup;
-			$upost['filetokens'] = $filetokens;
-			
-			$page = geturl("www.przeklej.pl", 80, "/ndodaj", "http://www.przeklej.pl/", $cookies, $upost, 0, "");
-			is_page($page);
 
-			$fileids = cut_str($page,'name="file[fileids]" value="','"');
-			
-			$epost['filetokens'] = $filetokens;
-			$epost['fileids%5Bfileids%5D'] = $fileids;
-			$epost['file%5Bdescription_'.$fileids.'%5D'] = "";
-			$epost['privacy-status'] = 1;
-			$epost['file%5Bhaslo_pliki%5D'] = "";
-			$epost['email'] = 0;
-			$epost['file%5Bemail%5D'] = "";
-			$epost['directory-val'] = 0;
-			$epost['file%5Bfoldername%5D'] = "";
-			$epost['file%5Bdescription%5D'] = "";
-			$epost['file%5Bhaslo%5D'] = "";
-			
-			$page = geturl("www.przeklej.pl", 80, "/dodaj_pliki", "http://www.przeklej.pl/", $cookies, $epost, 0, "");
+
+
+			$token=cut_str($upfiles, 'filetokens" value="', '"');
+			$fileads=cut_str($upfiles, 'file[fileids]" value="', '"');
+			unset($post);
+			$post['filetokens'] = $token;
+			$post['file[fileids]'] = $fileads;
+			$post['privacy-status'] = "0";
+			$post['file[haslo_pliki]'] = "";
+			$post['email'] = "0";
+			$post['file[email]'] = "";
+			$post['directory-val'] = "0";
+			$post['file[foldername]'] = "";
+			$post['file[description]'] = "";
+			$post['file[haslo]'] = "";
+			$url = parse_url("http://www.przeklej.pl/dodaj_pliki");
+			$page = geturl($url["host"], $url["port"] ? $url["port"] : 80, $url["path"].($url["query"] ? "?".$url["query"] : ""), $referrer, $cookie, $post, 0, $_GET["proxy"],$pauth);
 			is_page($page);
-			$name = strtr($lname , '.', '-');
-			$download_link = 'http://www.przeklej.pl/plik/'.$name.'-'.$fileidsup;
-// Made by Baking 16/10/2009 06:46
+		//	is_notpresent($page,"Files Uploaded","Error upload file",0);
+			$download_link="http://www.przeklej.pl/".cut_str($page, 'url=', '"');
+			
+			}
+
+/*************************\  
+Written by kaox 14-jun-2009
+Fixed by kaox 10-apr-2010
+\*************************/
 ?>
