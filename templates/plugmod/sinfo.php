930a39e8e1f364465e0d386d7b3aa76214bf6668
palooo.fs <palooo.fs@gmail.com>
2010-05-01 23:18:25 +0000

    Added CSS Definitions To Almost All Rapidleech Files. -RAR and UNRAR options table are now handled through CSS. -Server Space Info and CPU Info is now handled through CSS. -Credits are now handled through CSS. Really helpful for people to make styles for RL.

--- templates/plugmod/sinfo.php
+++ templates/plugmod/sinfo.php
@@ -1,20 +1,29 @@
 <hr />
-<table style="border-collapse: separate; border-spacing: 2px 2px;">
+<table class="ss-cpu-table">
 <tr>
-<td align="left" valign="top" style="color:#CCCCCC"><span style="color:#FF8700"><?php echo lang(275); ?>:</span><br />
+<!-- Start Server Space Info -->
+<td align="left" valign="top" class="ss-td-style"><span class="ss-span-style"><?php echo lang(275); ?>:</span><br />
 <?php echo lang(276); ?> = <b><span id="inuse"><?php echo ZahlenFormatieren($belegt); ?></span></b>&nbsp;(<span id="inusepercent"><?php echo round($prozent_belegt,"2"); ?></span> %)<br />
 <?php if (extension_loaded('gd') && function_exists('gd_info')) { ?>
 <img src="<?php echo CLASS_DIR ?>bar.php?rating=<?php echo round($prozent_belegt,"2"); ?>" border="0" name="diskpercent" id="diskpercent" alt="" /><br />
 <?php } ?>
 <?php echo lang(277); ?> = <b><span id="freespace"><?php echo ZahlenFormatieren($frei); ?></span></b><br />
-<?php echo lang(278); ?> = <b><span id="diskspace"><?php echo ZahlenFormatieren($insgesamt); ?></span></b></td>
-<td align="left" valign="top" style="color:#CCCCCC;width:200px"><span style="color:#FF8700;">
+<?php echo lang(278); ?> = <b><span id="diskspace"><?php echo ZahlenFormatieren($insgesamt); ?></span></b>
+</td>
+
+<!-- End Server Space -->
+
+
+<!-- Start CPU Info -->
+<td align="left" valign="top" class="cpu-td"><span class="cpu-span">
 <?php
 	if ($cpu_string === -1) { echo lang(135).'</span><hr />'; }
 	else { echo lang(279).':</span><br />'.$cpu_string; }
 ?>
-<span style="color:#FF8700"><?php echo lang(280); ?>:</span> &nbsp;&nbsp;&nbsp;<span style="color:#999"><span id="server"></span></span><br /><span id="clock"></span>
+<span class="cpu-clock-st-text"><?php echo lang(280); ?>:</span> &nbsp;&nbsp;&nbsp;<span class="cpu-clock-st-time"><span id="server"></span></span><br /><span id="clock"></span>
 </td>
+<!-- End CPU Info -->
+
 </tr>
 </table>
 <script type="text/javascript"> 
