74273bf6b7cc83b952222ed723337a27832ba1d2
malhotra1raj <malhotra1raj@gmail.com>
2010-05-16 17:30:58 +0000

    [Updated] Increased upload file size for free and member upload plug-in of easy-share

--- hosts/upload/easy-share.com.index.php
+++ hosts/upload/easy-share.com.index.php
@@ -1,5 +1,5 @@
 <?php 
 $upload_services[] = "easy-share.com";
-$max_file_size["easy-share.com"] = 200;
+$max_file_size["easy-share.com"] = 1000;
 $page_upload["easy-share.com"] = "easy-share.com.php";
 ?>
