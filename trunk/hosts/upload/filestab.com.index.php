c10c928a81bd6eae0bcb995da5b01ad9bd3290f7
christianduartecon <christianduartecon@0f90527e-9506-11de-8d2f-ebe662690e28>
2010-07-03 18:38:10 +0000

    [Add][PLUGIN] filestab.com member upload [Add][PLUGIN] filestab.com free upload

---
+++ trunk/hosts/upload/filestab.com.index.php
@@ -0,0 +1,5 @@
+<?php
+$upload_services[]="filestab.com";
+$max_file_size["filestab.com"]=2000;
+$page_upload["filestab.com"] = "filestab.com.php";
+?>
---
+++ trunk/hosts/upload/filestab.com_member.index.php
@@ -0,0 +1,5 @@
+<?php
+$upload_services[]="filestab.com_member";
+$max_file_size["filestab.com_member"]=2000;
+$page_upload["filestab.com_member"] = "filestab.com_member.php";
+?>
