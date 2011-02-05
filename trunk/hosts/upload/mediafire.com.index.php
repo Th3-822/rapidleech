e05e4a99f09966ba975949dca108bad8618909b1
malhotra1raj <malhotra1raj@0f90527e-9506-11de-8d2f-ebe662690e28>
2011-02-05 20:25:48 +0000

    [Added] mediafire.com member upload plugin

--- trunk/hosts/upload/mediafire.com.index.php
+++
@@ -1,5 +0,0 @@
-<?
-$upload_services[]="mediafire.com";
-$max_file_size["mediafire.com"]=200;
-$page_upload["mediafire.com"] = "mediafire.com.php";
-?>
---
+++ trunk/hosts/upload/mediafire.com_member.index.php
@@ -0,0 +1,10 @@
+<?php
+$upload_services[]="mediafire.com_member";
+$max_file_size["mediafire.com_member"]=200;
+$page_upload["mediafire.com_member"] = "mediafire.com_member.php";
+
+/******************mediafire.com****************************\
+mediafire.com Member Upload Plugin
+WRITTEN by Raj Malhotra on 06 Feb 2011
+\******************mediafire.com****************************/
+?>
