db91eab3f5873c438111f78a13e00dde1dc7cdb2
ruudvtony@gmail.com <ruudvtony@gmail.com>
2011-06-15 11:37:28 +0000

    [UPDATE] jQuery JavaScript Library to v.1.6.1 (js.js) [UPDATE] pear.php to 1.9.3 [REMOVE] Duckload download & upload plugin as the filehost closed now (sorry...too bad :() [REMOVE] storage.to download & upload plugin since it has changed into kickload n have been reported as scam site by lots of rapidleech forum member (again, I'm sorry...)

--- hosts/upload/duckload.com.index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$upload_services[]="duckload.com";
-$max_file_size["duckload.com"]=800;
-$page_upload["duckload.com"] = "duckload.com.php";
-?>
--- hosts/upload/storage.to.index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$upload_services[]="storage.to";
-$max_file_size["storage.to"]=1024;
-$page_upload["storage.to"] = "storage.to.php";  
-?>
