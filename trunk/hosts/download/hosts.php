396bbbb4323a81350f90a16d023dfb15c465cf91
ruudvtony@gmail.com <ruudvtony@gmail.com@0f90527e-9506-11de-8d2f-ebe662690e28>
2011-11-10 20:16:23 +0000

    [FIX] Table width in neatblue template, revert with the old one setting(haven't notice until I see server tab).

--- trunk/hosts/download/hosts.php
+++ trunk/hosts/download/hosts.php
@@ -8,6 +8,8 @@ while (false !== ($entry = $d->read())) {
 		if ($hostname == 'share.online.biz') $hostname = 'share-online.biz';
 		if ($hostname == 'cash.file.net') $hostname = 'cash-file.net';
 		if ($hostname == 'i.filez.com') $hostname = 'i-filez.com';
+		if ($hostname == 'fast.debrid.com') $hostname = 'fast-debrid.com';
+		if ($hostname == 'real.debrid.com') $hostname = 'real-debrid.com';
 		if ($hostname == 'generic.minifilehost' || $hostname == 'youtube.com(1)' || $hostname == 'vBulletin.plug' || $hostname == 'hosts') continue;
 		$host[$hostname] = $entry;
    }
