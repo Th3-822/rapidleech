c6a34fce773c0f36da14b75802e64e76486984e8
td-rl <thedevil.leech@gmail.com>
2016-12-12 22:11:42 +1000

    Removed some dead hosts (Download and Upload): - Hugefiles.net - Minus.com - Netkups.com

--- hosts/upload/minus.com.index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$upload_services[]="minus.com";
-$max_file_size["minus.com"]=2048;
-$page_upload["minus.com"] = "minus.com.php";  
-?>
--- hosts/upload/netkups.com.index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$upload_services[] = "netkups.com";
-$max_file_size["netkups.com"] = 1000;
-$page_upload["netkups.com"] = "netkups.com.php";
-?>
