4df472be92bf704520aa8d11745ac9894c2dae60
Th3-822 <Th3-822@users.noreply.github.com>
2016-12-29 08:50:34 -0400

    Fixed regexp for date format in rar5 < 5.30

--- classes/rar.php
+++ classes/rar.php
@@ -182,7 +182,7 @@ class rlRar {
 				}
 				$rar_onfile = !$rar_onfile;
 			} else {
-				if (preg_match('/^(\*)?(?:\s+[\w\.]{7}|(?(1)|\s)[dl\-](?:[r\-][w\-][x\-]){3})\s+(\d+)\s+\d+\s\d+%\s+[\w\-]{10}\s+[\d:]{4,5}\s+[\w\.]{8}\s+([^\r\n]+)$/', $rar_line, $attr)) {
+				if (preg_match('/^(\*)?(?:\s+[\w\.]{7}|(?(1)|\s)[dl\-](?:[r\-][w\-][x\-]){3})\s+(\d+)\s+\d+\s\d+%\s+(?:[\w\-]{10}|[\w\-]{8})\s+[\d:]{4,5}\s+[\w\.]{8}\s+([^\r\n]+)$/', $rar_line, $attr)) {
 					$rar_files[$attr[3]]['size'] = $attr[2];
 					if (!$rar_needs_pass && !empty($attr[1])) $rar_needs_pass = true;
 				}
