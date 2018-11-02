1b6049bfe85c498689d090892d8884dd2565e6f9
Nabi <nabikaz@gmail.com>
2018-11-02 06:04:13 +0330

    Fixed #161, Added aparat quality selector
    Signed-off-by: Nabi <nabikaz@gmail.com>

--- hosts/download/aparat_com.php
+++ hosts/download/aparat_com.php
@@ -6,25 +6,81 @@ if (!defined('RAPIDLEECH')) {
 }
 
 class aparat_com extends DownloadClass {
+	public $link;
+	
 	public function Download($link) {
+		$this->link = $link;
+		
 		if (!preg_match('@aparat\.com/v/(\w+)@i', $link, $vid)) html_error('Video ID not found.');
 		$vid = $vid[1];
 
 		$page = $this->GetPage("https://www.aparat.com/v/$vid");
 		is_present($page, "ویدیو مشابهی یافت نشد.", 'Video not found or it was deleted.');
 		if (!preg_match('@<title>(?>(.*?)</title>)@is', $page, $title)) html_error('Error: Video title not found.');
-		if (!preg_match('@https?://(?:[\w-]+\.)*aparat\.com/aparat-video/\w+(?:-(\d+p))?__\w+\.mp4@i', $page, $DL)) html_error('Download link not found.');
-
-		$filename = preg_replace('@[^ A-Za-z_\-\d\.,\(\)\[\]\{\}&\!\'\@\%\#]@u', '_', html_entity_decode(trim($title[1]), ENT_QUOTES, 'UTF-8'));
+		
+		$qualities = $this->Qualities($page);
+		
+		if (!isset($_GET['submit'])) {
+			echo $this->QSelector($qualities);
+			return;
+		}
+		
+		$quality = $qualities[$_GET['quality']]['quality'];
+		$video = $qualities[$_GET['quality']]['video'];
+		$filename = html_entity_decode(trim($title[1]), ENT_QUOTES, 'UTF-8');
+		if (isset($_GET['cleanname'])) {
+			$filename = preg_replace('@[^ A-Za-z_\-\d\.,\(\)\[\]\{\}&\!\'\@\%\#]@u', '_', $filename);
+		}
 		$filename = preg_replace('@(?:\.(?:mp4|flv|mkv|webm|wmv|(m2)?ts|rm(vb)?|mpe?g?|vob|avi|[23]gp))+$@i', '', $filename);
-		$filename .= sprintf(' [Aparat%s][%s].mp4', (empty($DL[1]) ? '' : '-' . $DL[1]), $vid);
+		$filename .= sprintf(' [Aparat%s][%s].mp4', (empty($quality) ? '' : '-' . $quality), $vid);
 
-		$this->RedirectDownload($DL[0], $filename, 0, 0, 0, $filename);
+		$this->RedirectDownload($video, $filename, 0, 0, 0, $filename);
+	}
+	
+	public function Qualities($page) {
+		if (!preg_match_all('@<a.*?href="(https?://(?:[\w-]+\.)*aparat\.com/aparat-video/\w+(?:-(\d+p))?__\w+\.mp4)".*?><span.*?>(.*?)<@im', $page, $matches, PREG_SET_ORDER, 0)) {
+			html_error('Download link not found.');
+		}
+		$return = array();
+		foreach ($matches as $match) {
+			$return[] = array(
+				'video' => $match[1],
+				'quality' => $match[2],
+				'title' => $match[3],
+			);
+		}
+		if (!$return) {
+			html_error('Download link not found.');
+		}
+		krsort($return);
+		return $return;
+	}
+	
+	public function QSelector($qualities) {
+		if (!is_array($qualities)) {
+			html_error('Quality not found.');
+		}
+		$str = "";
+		$str .= "\n<center><form action='{$_SERVER['SCRIPT_NAME']}' method='POST'>\n";
+		$str .= '<label><input type="checkbox" name="cleanname" value="1" /><small>&nbsp;Remove non-supported characters from filename</small></label><br />';
+		$str .= "<select name='quality'>\n";
+		foreach ($qualities as $key => $quality) {
+			$str .= "<option value='{$key}'>Quality: [" . ($quality['quality'] ? $quality['quality'] : 'Normal') . "]</option>\n";
+		}
+		$str .= "</select>\n";
+		$data = $this->DefaultParamArr($this->link);
+		foreach ($data as $n => $v) {
+			$str .= "<input type='hidden' name='$n' id='QS_$n' value='$v' />\n";
+		}
+		$str .= "<input type='submit' name='submit' value='".lang(209)."' />\n";
+		$str .= "</form></center>\n</body>\n</html>";
+		return $str;
 	}
 }
 
 //[23-12-2015]  Written by Th3-822.
 //[27-12-2015]  Fixed Regexp. - Th3-822
 //[02-11-2018]  Fixed video URL pattern - Nabi K.A.Z. <www.nabi.ir>
+//[02-11-2018]  Added aparat quality selector - Nabi K.A.Z. <www.nabi.ir>
 
 ?>
